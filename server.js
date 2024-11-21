f74b6e86f7af7a675153dad9f4ed40f5e3d9081f
syoh <syoh@qubicom.co.kr>
2024-11-21 10:51:30 +0900

    * 레이아웃 완성, gitignore 추가

--- server.js
+++ server.js
@@ -11,3 +11,21 @@ app.use(express.static("public"));
 app.get('/', (req, res) => { res.sendFile(__dirname + '/public/index.html'); });
 app.get("/write", (req, res) => { res.sendFile(__dirname + '/public/write.html') });
 app.get("/detail", (req, res) => { res.sendFile(__dirname + '/public/detail.html') });
+
+// MySQL
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: '172.25.161.119',
+    user: '< MySQL username >',
+    password: '< MySQL password >',
+    database: 'my_petit_note'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM note', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('Note: ', rows);
+});
+
+connection.end();
